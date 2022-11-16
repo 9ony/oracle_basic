af2704578a61c3ded57993e864efac788716b8aa
9ony <darsta7@gmail.com>
2022-11-17 01:14:16 +0900

    mybatis 테스트 구문작성  페이징 처리를위해 서브쿼리를 활용해 최근등록순으로 5개씩 select하는 구문 테스트

--- MvcWeb_sql/board.sql
+++ MvcWeb_sql/board.sql
@@ -16,3 +16,33 @@ create sequence board_seq
 start with 1
 increment by 1
 nocache;
+
+
+select * from board order by num desc;
+select * from board;
+commit;
+
+select board_seq.nextval from dual;
+
+
+select * from (
+    select A.*,rownum rn from 
+        (select board.* from board order by num desc) A
+)where rn between 1 and 5;
+--로우넘으로 페이징 처리를 위해 서브쿼리를 사용해서 select함
+--cpage(현재페이지)파라미터 값 받아서 DB에서 해당 cpage에 해당하는
+--데이터많 끊어서 가져오자=> SQL문 변경
+--
+--select * from(
+--select a.*, rownum rn from
+-- (select * from board order by num desc) a
+-- ) where rn between ${start} and ${end};
+-- 
+-- cpage   pageSize     start         end
+-- 1          5            1             5
+-- 2          5            6             10
+-- 3          5            11            15
+-- 4          5            16            20
+-- ...
+-- end= cpage * pageSize;
+-- start = end - (pageSize-1)
