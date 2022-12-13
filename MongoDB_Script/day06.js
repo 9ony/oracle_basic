5554ef50a044f48b5b1bb8d5190cb243365b4e0a
9ony <darsta7@gmail.com>
2022-12-14 01:32:24 +0900

    자바 몽고db 연동 연습

---
+++ MongoDB_Script/day06.js
@@ -0,0 +1,9 @@
+use mydb
+show collections
+db.posts.find()
+
+db.posts.insertMany([
+{author:"김삭제",title:"삭제테스트입니다",wdate:"2022-12-13"},
+{author:"김삭제",title:"삭제테스트입니다",wdate:"2022-12-13"},
+{author:"김삭제",title:"삭제테스트입니다",wdate:"2022-12-13"},
+{author:"김삭제",title:"삭제테스트입니다",wdate:"2022-12-13"}])
